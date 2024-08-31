da1a4104ffb859e9408688742e1f3c4a848594e9
filip2cz <filip@fkomarek.eu>
2024-08-31 21:48:39 +0200

    ADD: název a autor do title stránky

--- template/index.php
+++ template/index.php
@@ -26,7 +26,7 @@ $escaped_text = htmlspecialchars($text);
 <html lang='cs' data-bs-theme="dark">
 
 <head>
-    <title>zpěvník</title>
+    <title>Zpěvník: <?php echo htmlspecialchars($nazev); ?> - <?php echo htmlspecialchars($autor); ?></title>
     <link rel="icon" type="image/x-icon" href="./favicon.ico">
     <meta charset='utf-8'>
 </head>
