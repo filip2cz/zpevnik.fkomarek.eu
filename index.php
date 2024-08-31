7668f78cb6016dfedbfa05c1dfd42ed99cfb4069
filip2cz <filip@fkomarek.eu>
2024-09-01 00:22:44 +0200

    ADD: generovaný seznam písniček

--- index.php
+++ index.php
@@ -86,11 +86,6 @@ usort($songs, function($a, $b) {
             jako výchozí, opuštění cookies
         </p>
 
-        <!-- https://cs.wikipedia.org/wiki/Abeceda#%C4%8Cesk%C3%A1_abeceda -->
-
-
-        <!--
-
         <ul>
             <?php foreach ($songs as $song): ?>
                 <li class="songs-list"><a
@@ -98,46 +93,6 @@ usort($songs, function($a, $b) {
                 </li>
             <?php endforeach; ?>
         </ul>
-
-        -->
-
-        <!-- <p class="center"><a href="odkaz">nazev</a></p> -->
-
-        <p class="center"><a href="bastila/">Bastila</a></p>
-
-        <p class="center"><a href="batalion/">Batalion</a></p>
-
-        <p class="center"><a href="dlazebni-kostka/">Dlažební kostka </a></p>
-
-        <p class="center"><a href="drak/">Drak</a></p>
-
-        <p class="center"><a href="jdou-po-mne-jdou/">Jdou po mně jdou</a></p>
-
-        <p class="center"><a href="kluziste/">Kluziště</a></p>
-
-        <p class="center"><a href="kockoholky/">Kočkoholky</a></p>
-
-        <p class="center"><a href="letecky-most/">Letecký most</a></p>
-
-        <p class="center"><a href="batalion/">Markytánka</a></p>
-
-        <p class="center"><a href="planeta-hieronyma-bosche-ii/">Planeta Hieronyma Bosche II</a></p>
-
-        <p class="center"><a href="rada-se-miluje/">Ráda se miluje</a></p>
-
-        <p class="center"><a href="slavici/">Slavíci z Madridu</a></p>
-
-        <p class="center"><a href="soutez/">Soutěž</a></p>
-
-        <p class="center"><a href="stanky/">Stánky</a></p>
-
-        <p class="center"><a href="tri-krize/">Tři kříže</a></p>
-
-        <p class="center"><a href="tri-krize-hakovy/">Tři kříže (hákový)</a></p>
-
-        <p class="center"><a href="batalion/">Tourdion</a></p>
-
-        <p class="center"><a href="zlaty-hrebik-noci/">Zlatý hřebík noci</a></p>
 
         <br>
 
