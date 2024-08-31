3e823ae4c67d8629da7800c9f2cb2e37209c0d78
filip2cz <filip@fkomarek.eu>
2024-08-31 21:56:38 +0200

    EDIT: oddělení css a bootstrap do samostatných souborů

--- template/index.php
+++ template/index.php
@@ -32,17 +32,9 @@ $escaped_text = htmlspecialchars($text);
 </head>
 
 <body>
-    <!-- CSS a JavaScript -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-        crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="../main.css">
-    <link rel="stylesheet" type="text/css" href="../amoled.css" id="dark-mode-stylesheet">
+    <?php include("../include/bootstrap.html") ?>
 
-    <!-- Support pro mobilní menu -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include("../include/css.html") ?>
 
     <div class="container">
 
