1a0a1916e19eec3a4bb18a8056cbb4aa52ff47bc
filip2cz <filip@fkomarek.eu>
2024-09-03 00:52:09 +0200

    ADD: možnost dvou alternativních platform

--- include/song.php
+++ include/song.php
@@ -22,6 +22,10 @@ $soundcloud = isset($song['soundcloud']) ? $song['soundcloud'] : '';
 $soundcloudText = isset($song['soundcloudText']) ? $song['soundcloudText'] : 'SoundCloud';
 $spotify = isset($song['spotify']) ? $song['spotify'] : '';
 $spotifyText = isset($song['spotifyText']) ? $song['spotifyText'] : 'Spotify';
+$genericPlatform1 = isset($song['genericPlatform1']) ? $song['genericPlatform1'] : '';
+$genericPlatform1Text = isset($song['genericPlatform1Text']) ? $song['genericPlatform1Text'] : 'Další platforma 1';
+$genericPlatform2 = isset($song['genericPlatform2']) ? $song['genericPlatform2'] : '';
+$genericPlatform2Text = isset($song['genericPlatform2Text']) ? $song['genericPlatform2Text'] : 'Další platforma 2';
 
 // Načtení obsahu souboru text.txt
 $text = file_get_contents('text.txt');
@@ -67,22 +71,40 @@ $escaped_text = htmlspecialchars($text);
 
         <p class="center">
             <?php if (!empty($invidious)): ?>
-                <a href="<?php echo htmlspecialchars($invidious); ?>" target="_blank"><?php echo htmlspecialchars($invidiousText); ?></a>
+                <a href="<?php echo htmlspecialchars($invidious); ?>"
+                    target="_blank"><?php echo htmlspecialchars($invidiousText); ?></a>
             <?php endif; ?>
 
             <?php if (!empty($youtube)): ?>
                 <?php if (!empty($invidious)): ?> | <?php endif; ?>
-                <a href="<?php echo htmlspecialchars($youtube); ?>" target="_blank"><?php echo htmlspecialchars($youtubeText); ?></a>
+                <a href="<?php echo htmlspecialchars($youtube); ?>"
+                    target="_blank"><?php echo htmlspecialchars($youtubeText); ?></a>
             <?php endif; ?>
 
             <?php if (!empty($soundcloud)): ?>
                 <?php if (!empty($invidious) || !empty($youtube)): ?> | <?php endif; ?>
-                <a href="<?php echo htmlspecialchars($soundcloud); ?>" target="_blank"><?php echo htmlspecialchars($soundcloudText); ?></a>
+                <a href="<?php echo htmlspecialchars($soundcloud); ?>"
+                    target="_blank"><?php echo htmlspecialchars($soundcloudText); ?></a>
             <?php endif; ?>
 
             <?php if (!empty($spotify)): ?>
                 <?php if (!empty($invidious) || !empty($youtube) || !empty($soundcloud)): ?> | <?php endif; ?>
-                <a href="<?php echo htmlspecialchars($spotify); ?>" target="_blank"><?php echo htmlspecialchars($spotifyText); ?></a>
+                <a href="<?php echo htmlspecialchars($spotify); ?>"
+                    target="_blank"><?php echo htmlspecialchars($spotifyText); ?></a>
+            <?php endif; ?>
+
+            <?php if (!empty($genericPlatform1)): ?>
+                <?php if (!empty($invidious) || !empty($youtube) || !empty($soundcloud) || !empty($spotify)): ?> |
+                <?php endif; ?>
+                <a href="<?php echo htmlspecialchars($genericPlatform1); ?>"
+                    target="_blank"><?php echo htmlspecialchars($genericPlatform1Text); ?></a>
+            <?php endif; ?>
+
+            <?php if (!empty($genericPlatform2)): ?>
+                <?php if (!empty($invidious) || !empty($youtube) || !empty($soundcloud) || !empty($spotify) || !empty($genericPlatform1)): ?>
+                    | <?php endif; ?>
+                <a href="<?php echo htmlspecialchars($genericPlatform2); ?>"
+                    target="_blank"><?php echo htmlspecialchars($genericPlatform2Text); ?></a>
             <?php endif; ?>
         </p>
 
